57ba52889afcd6b9d8a111c2a87046f8bcd5a736
Michael Soares <mikey.soares@gmail.com>
2018-10-31 15:23:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,9 +18,7 @@ end
  def input_to_index(input)
   input = input.to_i
   if input.between?(1,9)
-    input -= 1
-  else
-    input = -1
+  index = - 1
   end
 end
  def move(board, index, current_player)
